e65bf916c2c9e5b6201b9b1ec6a49731cfc84b93
rick <rick@ricks-MacBook-Air.local>
2020-10-05 18:44:44 -0400

    reference properties properly

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -144,10 +144,13 @@ model.save(args["model"], save_format="h5")
 N = EPOCHS
 plt.style.use("ggplot")
 plt.figure()
+
+print('H history keys = {}'.format(dict(H.history).keys()))
+
 plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
 plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
 plt.title("Training Loss and Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
