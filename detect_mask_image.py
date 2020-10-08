1aa873eab2257cb649126e2ddeeaabfa94f3cc33
rick <rick@ricks-MacBook-Air.local>
2020-10-08 18:52:02 -0400

    working images handler

--- detect_mask_image.py
+++ detect_mask_image.py
@@ -51,7 +51,7 @@ def mask_image():
 
         if args["images"]:
           print(f'handling images')
-          IMAGES = args["images"]
+          IMAGES = args["images"].split(',')
         else:
           IMAGES = [args["image"]]
 
@@ -68,87 +68,91 @@ def mask_image():
 
 
         ##  for each image:
+        print(f'IMAGES={IMAGES}')
+        for img in IMAGES:
+            args["image"] = img
+            print(f'img={img}')
+            image = cv2.imread(img)
+            # load the input image from disk, clone it, and grab the image spatial
+            # dimensions
+            image = cv2.imread(img)
+            orig = image.copy()
+            (h, w) = image.shape[:2]
 
-        # load the input image from disk, clone it, and grab the image spatial
-        # dimensions
-        image = cv2.imread(args["image"])
-        orig = image.copy()
-        (h, w) = image.shape[:2]
+            # construct a blob from the image
+            blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+                    (104.0, 177.0, 123.0))
 
-        # construct a blob from the image
-        blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
-                (104.0, 177.0, 123.0))
+            # pass the blob through the network and obtain the face detections
+            print("[INFO] computing face detections...")
+            net.setInput(blob)
+            detections = net.forward()
 
-        # pass the blob through the network and obtain the face detections
-        print("[INFO] computing face detections...")
-        net.setInput(blob)
-        detections = net.forward()
+            # loop over the detections
+            for i in range(0, detections.shape[2]):
+                    # extract the confidence (i.e., probability) associated with
+                    # the detection
+                    confidence = detections[0, 0, i, 2]
 
-        # loop over the detections
-        for i in range(0, detections.shape[2]):
-                # extract the confidence (i.e., probability) associated with
-                # the detection
-                confidence = detections[0, 0, i, 2]
+                    # filter out weak detections by ensuring the confidence is
+                    # greater than the minimum confidence
+                    if confidence > args["confidence"]:
+                            # compute the (x, y)-coordinates of the bounding box for
+                            # the object
+                            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                            (startX, startY, endX, endY) = box.astype("int")
 
-                # filter out weak detections by ensuring the confidence is
-                # greater than the minimum confidence
-                if confidence > args["confidence"]:
-                        # compute the (x, y)-coordinates of the bounding box for
-                        # the object
-                        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                        (startX, startY, endX, endY) = box.astype("int")
+                            # ensure the bounding boxes fall within the dimensions of
+                            # the frame
+                            (startX, startY) = (max(0, startX), max(0, startY))
+                            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-                        # ensure the bounding boxes fall within the dimensions of
-                        # the frame
-                        (startX, startY) = (max(0, startX), max(0, startY))
-                        (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+                            # extract the face ROI, convert it from BGR to RGB channel
+                            # ordering, resize it to 224x224, and preprocess it
+                            face = image[startY:endY, startX:endX]
+                            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                            face = cv2.resize(face, (224, 224))
+                            face = img_to_array(face)
+                            face = preprocess_input(face)
+                            face = np.expand_dims(face, axis=0)
 
-                        # extract the face ROI, convert it from BGR to RGB channel
-                        # ordering, resize it to 224x224, and preprocess it
-                        face = image[startY:endY, startX:endX]
-                        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-                        face = cv2.resize(face, (224, 224))
-                        face = img_to_array(face)
-                        face = preprocess_input(face)
-                        face = np.expand_dims(face, axis=0)
+                            # pass the face through the model to determine if the face
+                            # has a mask or not
+                            (mask, withoutMask) = model.predict(face)[0]
 
-                        # pass the face through the model to determine if the face
-                        # has a mask or not
-                        (mask, withoutMask) = model.predict(face)[0]
+                            # determine the class label and color we'll use to draw
+                            # the bounding box and text
+                            label = "Mask" if mask > withoutMask else "No Mask"
+                            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
-                        # determine the class label and color we'll use to draw
-                        # the bounding box and text
-                        label = "Mask" if mask > withoutMask else "No Mask"
-                        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+                            # include the probability in the label
+                            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-                        # include the probability in the label
-                        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+                            confidence = "{:.2f}".format(max(mask, withoutMask) * 100)
+                            mask_detected = (True if mask > withoutMask else False)
+                            img_extension = img.split('.')[-1]
+                            RESULTS_DIR = MASK_DIR if mask > withoutMask else NO_MASK_DIR
+                            dst_image = '{}/{}_{}_{}.{}'.format(
+                                RESULTS_DIR,
+                                os.path.basename(img).replace('.{}'.format(img_extension),''),
+                                'face',
+                                i+1,
+                                img_extension,
+                            )
+                            clone = image.copy()
+                            cv2.putText(clone, label, (startX, startY - 10),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                            cv2.rectangle(clone, (startX, startY), (endX, endY), color, 2)
+                            cropped_img = clone[startY:endY, startX:endX]
+    #                        cv2.imwrite(dst_image, clone)
+                            cv2.imwrite(dst_image, cropped_img)
+                            record_face(startX, startY, endX, endY, confidence, mask_detected, img, dst_image)
 
-                        confidence = "{:.2f}".format(max(mask, withoutMask) * 100)
-                        mask_detected = (True if mask > withoutMask else False)
-                        img_extension = args["image"].split('.')[-1]
-                        RESULTS_DIR = MASK_DIR if mask > withoutMask else NO_MASK_DIR
-                        dst_image = '{}/{}_{}_{}.{}'.format(
-                            RESULTS_DIR,
-                            os.path.basename(args["image"]).replace('.{}'.format(img_extension),''),
-                            'face',
-                            i+1,
-                            img_extension,
-                        )
-                        clone = image.copy()
-                        cv2.putText(clone, label, (startX, startY - 10),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-                        cv2.rectangle(clone, (startX, startY), (endX, endY), color, 2)
-                        cropped_img = clone[startY:endY, startX:endX]
-#                        cv2.imwrite(dst_image, clone)
-                        cv2.imwrite(dst_image, cropped_img)
-                        record_face(startX, startY, endX, endY, confidence, mask_detected, args["image"], dst_image)
-
-                        # display the label and bounding box rectangle on the output
-                        # frame
-                        cv2.putText(image, label, (startX, startY - 10),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-                        cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+                            # display the label and bounding box rectangle on the output
+                            # frame
+                            cv2.putText(image, label, (startX, startY - 10),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                            cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
 
         # show the output image
         #print(image)
