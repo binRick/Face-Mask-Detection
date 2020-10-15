a7d3016bd24b51c1b8c8df1e177c39156fee5c0b
rick <rick@ricks-MacBook-Air.local>
2020-10-15 17:57:14 -0400

    remove useless imgcat idea

--- detect_mask_image.sh
+++ detect_mask_image.sh
@@ -5,17 +5,4 @@ export PATH="$(pwd)/bin:$PATH"
 
 args=""
 cmd="./detect_mask_image.py $args $@ 2>/dev/null"
-if [[ "1" == "0" ]]; then
- while read -r line; do
-  echo -e "GOT LINE '$line'"
-  if [[ "$line" == *"dst_image"* ]]; then
-    echo -e "  extracting dst_image...."
-    dst_image_cmd="echo -e "$line"|jq '.dst_image' -Mrc"
-    dst_image="$(eval $dst_image_cmd)"
-    echo -e "  $dst_image_cmd"
-    echo -e "  $dst_image"
-  fi
- done < <(eval $cmd)
-else
- eval $cmd
-fi
+eval $cmd
