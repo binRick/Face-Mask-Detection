c8749d5dca80159fa621576884cc23666eb26427
rick <rick@ricks-MacBook-Air.local>
2020-10-15 20:14:14 -0400

    add auto installer

--- source.sh
+++ source.sh
@@ -1,2 +1,5 @@
-
+[[ -e ./.v/bin/activate ]] || python3 -m venv .v
 source ./.v/bin/activate
+
+pip freeze | grep -q tensorflow ||  \
+                                    pip install -r requirements.txt
