ccbccc601041d6e0b38b2c1091abc2dfeee76777
rick <rick@ricks-MacBook-Air.local>
2020-10-19 19:52:37 -0400

    add rtsp server

--- source.sh
+++ source.sh
@@ -1,6 +1,8 @@
 [[ -e ./.v/bin/activate ]] || python3 -m venv .v
 source ./.v/bin/activate
 
+[[ ! -f ./rtsp-simple-server ]] || {wget https://github.com/aler9/rtsp-simple-server/releases/download/v0.10.1/rtsp-simple-server_v0.10.1_darwin_amd64.tar.gz; }
+
 command -v ffmpeg >/dev/null || { 
   sudo dnf -y install epel-release dnf-utils; \
   sudo yum-config-manager --set-enabled PowerTools; \
