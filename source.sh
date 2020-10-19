4b8ffd0702cb419c56a2e4608c2df32b2d781505
Your Name <you@example.com>
2020-10-19 13:50:37 -0400

    add ffmpeg installer

--- source.sh
+++ source.sh
@@ -1,5 +1,12 @@
 [[ -e ./.v/bin/activate ]] || python3 -m venv .v
 source ./.v/bin/activate
 
+command -v ffmpeg >/dev/null || { 
+  sudo dnf -y install epel-release dnf-utils; \
+  sudo yum-config-manager --set-enabled PowerTools; \
+  sudo yum-config-manager --add-repo=https://negativo17.org/repos/epel-multimedia.repo; \
+  sudo dnf -y install ffmpeg;
+}
+
 pip freeze | grep -q tensorflow ||  \
                                     pip install -r requirements.txt
