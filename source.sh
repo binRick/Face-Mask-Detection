d3fa126efda6aaa426d4c52390ebda7b68e90d83
Your Name <you@example.com>
2020-10-20 12:51:24 -0400

    working installer for linux

--- source.sh
+++ source.sh
@@ -1,7 +1,10 @@
 [[ -e ./.v/bin/activate ]] || python3 -m venv .v
 source ./.v/bin/activate
 
-[[ -f ./rtsp-simple-server ]] || { wget https://github.com/aler9/rtsp-simple-server/releases/download/v0.10.1/rtsp-simple-server_v0.10.1_darwin_amd64.tar.gz; }
+[[ -d .tmp ]] && rm -rf .tmp
+
+[[ ! -f ./rtsp-simple-server ]] && uname -a | grep -qi linux && { echo Linux; wget https://github.com/aler9/rtsp-simple-server/releases/download/v0.10.1/rtsp-simple-server_v0.10.1_linux_amd64.tar.gz; mkdir .tmp; cd .tmp; tar zxvf ../rtsp-simple-server_v0.10.1_linux_amd64.tar.gz; mv rtsp-simple-server ../; cd ../; rm -rf .tmp;} 
+[[ ! -f ./rtsp-simple-server ]] && uname -a | grep -qi darwin && { wget https://github.com/aler9/rtsp-simple-server/releases/download/v0.10.1/rtsp-simple-server_v0.10.1_darwin_amd64.tar.gz; mkdir .tmp; cd .tmp; tar zxvf ../rtsp-simple-server_v0.10.1_darwin_amd64.tar.gz; mv rtsp-simple-server ../; cd ../; rm -rf .tmp; }
 
 command -v ffmpeg >/dev/null || { 
   sudo dnf -y install epel-release dnf-utils; \
