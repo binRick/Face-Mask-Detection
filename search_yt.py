9f0b9c32d4d67c47aeafef39470d0cf4e4859fdc
rick <rick@ricks-MacBook-Air.local>
2020-10-21 00:26:25 -0400

    working video analyzer

---
+++ search_yt.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os, json, sys, time
+from youtubesearchpython import SearchVideos
+
+def get_ids():
+  with open('youtube_video_ids.txt','r') as f:
+    return f.read().splitlines()
+
+max_results = 2000
+on = 0
+step = 50
+while on < max_results:
+  search = SearchVideos("mask styles", offset = on+1, mode = "json", max_results = on+50)
+  for r in dict(json.loads(search.result()))['search_result']:
+    mat = (str(r['id']) in get_ids())
+    print(r['id'], get_ids(), mat)
+    if not str(r['id']) in get_ids():
+      with open('youtube_video_ids.txt','a') as f:
+        f.write(str(r['id'])+"\n")
+  on += step
+  print(get_ids())
+  
+  time.sleep(1)
+#print(dict(json.loads(search.result()))['search_result'][0])
