aad6ad121e74ed3b7501e9538db19df7f8169420
rick <rick@ricks-MacBook-Air.local>
2020-10-21 18:30:46 -0400

    m

--- fetch_videos.sh
+++ fetch_videos.sh
@@ -1,14 +1,43 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+set -e
+source concurrent.lib.sh
 YTARGS="--write-info-json --no-mtime --write-thumbnail  -q"
+MAX_QTY=2
+get_ids(){
+  cat youtube_video_ids.txt|sort -u|shuf|head -n $MAX_QTY
+}
+#|xargs -n 3 -I % echo -e "% youtube-dl $YTARGS --merge-output-format mkv --max-filesize 500m --limit-rate 2M -o videos/% https://www.youtube.com/watch?v=%"
+#}
 
-get_cmds(){
-  cat youtube_video_ids.txt|xargs -n 2 -I % echo -e "youtube-dl $YTARGS --merge-output-format mkv --max-filesize 500m --limit-rate 2M -o videos/% https://www.youtube.com/watch?v=%"
+
+
+#concurrent \
+#    - 'My long task'   sleep 1 \
+#    - 'My medium task' sleep 1  \
+#    - 'My short task'  sleep 1  \
+#    --require 'My short task' --before-all
+
+youtube_dl(){
+  echo -e "$@" >> /tmp/.n
 }
 
-while read -r cmd; do 
-  eval $cmd
-done < <(get_cmds)
+gen_concurrent(){
+ echo -e "#!/usr/bin/env bash\nsource $(pwd)/concurrent.lib.sh"
+ echo -e "youtube_dl(){ youtube-dl $YTARGS --merge-output-format mkv --max-filesize 500m --limit-rate 2M https://www.youtube.com/watch?v=\$1; }"
+ echo -e "concurrent \\"
+ echo -e "  - 'My short task' echo OK \\"
+ while read -r id; do 
+  echo -e "  - '$id' youtube_dl '$id' \\"
+#  echo -e "  --and-then \\"
+ done < <(get_ids)
+ echo -e " --require 'My short task' --before-all"
+}
+
+gen_concurrent > /tmp/r
+chmod +x /tmp/r
+/tmp/r
+
+exit
 
 sleep 10
 
--- search_yt.py
+++ search_yt.py
@@ -2,23 +2,33 @@
 import os, json, sys, time
 from youtubesearchpython import SearchVideos
 
-def get_ids():
-  with open('youtube_video_ids.txt','r') as f:
-    return f.read().splitlines()
+SEARCH_TERMS = ['mask styles','how to wear mask','face mask','facemask','n95 mask','my mask']
+ids_file = 'youtube_video_ids.txt'
 
-max_results = 2000
-on = 0
-step = 50
-while on < max_results:
-  search = SearchVideos("mask styles", offset = on+1, mode = "json", max_results = step)
-  for r in dict(json.loads(search.result()))['search_result']:
-    mat = (str(r['id']) in get_ids())
-    print(r['id'], get_ids(), mat)
-    if not str(r['id']) in get_ids():
-      with open('youtube_video_ids.txt','a') as f:
-        f.write(str(r['id'])+"\n")
-  on += step
-  print(get_ids())
-  
-  time.sleep(1)
-#print(dict(json.loads(search.result()))['search_result'][0])
+def get_ids():
+  with open(ids_file, 'r') as f:
+    return [str(l) for l in f.read().splitlines() if len(str(l)) > 0]
+
+acquired_ids = []
+DELAY = 0.01
+
+for S in SEARCH_TERMS:
+    max_results = 200
+    on = 0
+    step = 20
+    while on < max_results:
+      offset = on + 1
+      print(f'  on={on}, max={max_results}, step={step}, offset={offset}, term={S}, ')
+      search = SearchVideos(S, offset = offset, mode = "json", max_results = step)
+      on += step
+      ids = [r['id'] for r in dict(json.loads(search.result()))['search_result'] if 'id' in r.keys()]
+      for i in ids:
+        if not i in acquired_ids:
+          acquired_ids.append(i)
+      cur_ids = get_ids()
+      print(f'  {len(acquired_ids)} acquired_ids, ids={len(ids)}, cur_ids qty={len(cur_ids)}, ')
+      for i in acquired_ids:
+        if not i in get_ids():
+          with open(ids_file, 'a') as f:
+            f.write(i+'\n')
+      time.sleep(DELAY)
